e3640c2efe524b6d84d3dc6fdb675df47fee47bb
dazia <abdelbarredazia@gmail.com>
2022-05-23 09:14:28 +0100

    update for login

---
+++ components/maps/MapsComponets.js
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet, Platform } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps';
+export default function MapsComponets({latitude, longitude}) {
+  return (
+    <View>
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude:latitude,
+          longitude:longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+
+      >
+        <MapView.Marker
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+
+        />
+        </MapView>
+        </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    }
+  });
+  
