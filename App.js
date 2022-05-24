d1b2dd92b6d3ab5c0904b6338c8b7556616ef567
dazia <abdelbarredazia@gmail.com>
2022-05-24 09:49:19 +0100

    update in login

--- App.js
+++ App.js
@@ -5,44 +5,49 @@ import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import LoginScreen from 'react-native-login-screen';
+import LoginScreens from './screen/LoginScreens';
+//import Newaccount from './screen/Newaccount';
+
+
+
 export default function App() {
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+//   const [errorMessage, setErrorMessage] = useState(null);
+//   const [latitude, setLatitude] = useState(null);
+//   const [longitude, setLongitude] = useState(null);
 
-useEffect(() => {
-  load()
-},[latitude, longitude]);
-
-
-  const load = async () => {
-
-    try {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMessage('Access to location is needed to run this app');
-        return
-      }
-      const location = await Location.getCurrentPositionAsync();
-
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude);
-    } catch (error) {
-
-      setErrorMessage(error.message);
-    }
-  }
+// useEffect(() => {
+//   load()
+// },[latitude, longitude]);
 
 
+//   const load = async () => {
+
+//     try {
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         setErrorMessage('Access to location is needed to run this app');
+//         return
+//       }
+//       const location = await Location.getCurrentPositionAsync();
+
+//       setLatitude(location.coords.latitude);
+//       setLongitude(location.coords.longitude);
+//     } catch (error) {
+
+//       setErrorMessage(error.message);
+//     }
+//   }
 
 
-if(latitude&&longitude){
+
+
+// if(latitude&&longitude){
 
   return (
     <View style={styles.container}>
+   
       {/* Render our MapView*/}
-      <MapView
+      {/* <MapView
         style={styles.map}
         showsMyLocationButton={true}
         //specify our coordinates.
@@ -63,8 +68,8 @@ if(latitude&&longitude){
           }}
 
         />
-      </MapView>
-      {/* <LoginScreen/> */}
+      </MapView> */}
+      <LoginScreens/>
  
     </View>
 
@@ -72,18 +77,18 @@ if(latitude&&longitude){
 
   );
  }
- else{
-    return(
+//  else{
+//     return(
   
-        <View style={styles.loading}>
-          <ActivityIndicator size="large" />
-          <StatusBar style="auto" />
-        </View>
+//         <View style={styles.loading}>
+//           <ActivityIndicator size="large" />
+//           <StatusBar style="auto" />
+//         </View>
 
-    );
-  }
+//     );
+//   }
 
- }
+//  }
 
  
 
